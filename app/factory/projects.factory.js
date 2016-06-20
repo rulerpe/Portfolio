96c96dfce42dfb60390641ecbf61b5e3e83df392
Peter <neetisthebest@gmail.com>
2016-06-19 20:02:47 -0700

    add directive and factory

---
+++ app/factory/projects.factory.js
@@ -0,0 +1,19 @@
+angular
+	.module('portfolioApp')
+	.factory('projectsFactory', projectsFactory);
+
+projectsFactory.$inject = ['$http'];
+
+function projectFactory($http){
+	return {
+		all: all,
+		get: get
+	};
+
+	function all() {
+		return $http.get('/api/projects');
+	}
+	function get(id) {
+		return $http.get('/api/projects' + id);
+	}
+}
